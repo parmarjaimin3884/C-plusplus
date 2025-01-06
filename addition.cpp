b628b81e966e6f69571a941947ef7c8d6a692221
jaimin parmar <parmarjaimin3884@gmail.com>
2025-01-06 18:36:15 +0530

    addition in C++

---
+++ addition.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,sum;
+    cout << "Enter 1st value: ";
+    cin >> a;
+    cout << "Enter 2nd value: ";
+    cin >> b;
+    sum = a+b;
+    cout << "The sum of is: " << sum;
+
+
+
+}
